fefd06462454a1572a6abef23e345483e0ad8337
ElcioLS <elciols@gmail.com>
2022-08-27 00:03:15 -0300

    ObservableFuture Parte 2

--- lib/observables/future/future_controller.dart
+++ lib/observables/future/future_controller.dart
@@ -7,11 +7,13 @@ class FutureController = FutureControllerBase with _$FutureController;
 abstract class FutureControllerBase with Store {
   @observable
   // ObsevableFuture nomeFuture = ObservableFuture(Future.value()); //jeito incorreto
-  var nomeFuture = Future.value('').asObservable();
+  // var nomeFuture = Future.value('').asObservable();
+  var nomeFuture = ObservableFuture.value('');
 
   Future<void> buscarNome() async {
-    nomeFuture = Future.delayed(const Duration(seconds: 2), () => 'Elcinho')
-        .asObservable();
-    // return 'Elcinho';
+    final buscaNomeFuture =
+        Future.delayed(const Duration(seconds: 2), () => 'Elcinho');
+    // nomeFuture = buscaNomeFuture.asObservable(); //pode ser assim
+    nomeFuture = ObservableFuture(buscaNomeFuture); //assim fica mais simples
   }
 }
